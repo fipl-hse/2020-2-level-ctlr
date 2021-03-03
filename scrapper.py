fd1dffe4654c68d66d2fae83c5b6325127ce55d3
makar-fedorov <65069232+makar-fedorov@users.noreply.github.com>
2021-03-03 22:04:35 +0500

    scrapper: searching in seed

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,13 @@
 """
 Crawler implementation
 """
-
+import json
+import requests
+import random
+import re
+from constants import CRAWLER_CONFIG_PATH
+from time import sleep
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'}
 
 class IncorrectURLError(Exception):
     """
@@ -21,12 +27,21 @@ class IncorrectNumberOfArticlesError(Exception):
     """
 
 
+class UnknownConfigError(Exception):
+    """
+    Custom error
+    """
+
+
 class Crawler:
     """
     Crawler implementation
     """
-    def __init__(self, seed_urls: list, max_articles: int):
-        pass
+    def __init__(self, seed_urls: list, max_articles: int, max_articles_per_seed: int):
+        self.seed_urls = seed_urls
+        self.total_max_articles = max_articles
+        self.max_articles_per_seed = max_articles_per_seed
+        self.urls = []
 
     @staticmethod
     def _extract_url(article_bs):
@@ -36,7 +51,18 @@ class Crawler:
         """
         Finds articles
         """
-        pass
+        raw_urls_list = []
+        for url in self.seed_urls:
+            response = requests.get(url, headers=headers)
+            print('Making a request...')
+            sleep_interval = random.randrange(2, 5)
+            sleep(sleep_interval)
+            text = response.text
+            link_sample = re.compile(r'<h3><a href="/\d{6}.+"')
+            raw_urls_list.extend(re.findall(link_sample, text))
+        for url in raw_urls_list:
+            self.urls.append(url.replace('<h3><a href="', 'https://www.infpol.ru')[:-1])
+
 
     def get_search_urls(self):
         """
@@ -83,9 +109,31 @@ def validate_config(crawler_path):
     """
     Validates given config
     """
-    pass
+    with open(crawler_path, 'r') as crawler:
+        data = json.loads(str(crawler.read()))
+        validating_seed_urls = data["base_urls"]
+        validating_max_articles = data["total_articles_to_find_and_parse"]
+        validating_max_articles_per_seed = data["max_number_articles_to_get_from_one_seed"]
+
+    if not isinstance(validating_seed_urls, list) or not isinstance(validating_max_articles, int)\
+            or not isinstance(validating_max_articles_per_seed, int):
+        raise UnknownConfigError
+
+    for url in validating_seed_urls:
+        response = requests.get(url)
+        if not response:
+            raise IncorrectURLError
+
+    '''if validating_max_articles == 0:
+        raise NumberOfArticlesOutOfRangeError
+
+    if validating_max_articles_per_seed == 0:
+        raise IncorrectNumberOfArticlesError'''
+    return validating_seed_urls, validating_max_articles, validating_max_articles_per_seed
 
 
 if __name__ == '__main__':
-    # YOUR CODE HERE
+    seed_urls, max_articles, max_articles_per_seed = validate_config(CRAWLER_CONFIG_PATH)
+    example = Crawler(seed_urls, max_articles, max_articles_per_seed)
+    example.find_articles()
     pass
