6d48e11ab41bfc470c0b42fd0808da7ba2dfcd12
Darya-borisenko <65235764+Darya-borisenko@users.noreply.github.com>
2021-03-10 20:46:26 +0300

    starting changes

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,15 @@
 import requests
 import json
+import re
 from time import sleep
+from bs4 import BeautifulSoup
+from constants import CRAWLER_CONFIG_PATH
+
+headers = {
+        'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/88.0.4324.111 YaBrowser/21.2.1.108 Yowser/2.5 Safari/537.36'
+
+}
 
 
 """
@@ -36,29 +45,27 @@ class Crawler:
     """
     Crawler implementation
     """
-    def __init__(self, seed_urls: list, max_articles: int):
+    def __init__(self, seed_urls: list, max_articles: int, max_articles_per_seed: int):
         self.seed_urls = seed_urls
         self.max_articles = max_articles
+        self.max_articles_per_seed = max_articles_per_seed
         self.urls = []
 
 
     @staticmethod
     def _extract_url(article_bs):
-        pass
+        url_article = article_bs.find(id="MainMasterContentPlaceHolder_DefaultContentPlaceHolder_ctl02_ctl00_articleHeader").find('a')
+        return url_article.attr(['href'])
 
     def find_articles(self):
         """
         Finds articles
         """
-        headers = {
-            'user-agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.111 YaBrowser/21.2.1.108 Yowser/2.5 Safari/537.36'
-
-        }
         for url in self.seed_urls:
             response = requests.get(url, headers=headers)
-            print('Making a request...')
             sleep(5)
-            text = response.text
+            if not response:
+                raise IncorrectURLError
 
 
 
@@ -108,13 +115,22 @@ def validate_config(crawler_path):
     """
     Validates given config
     """
+    with open(crawler_path) as crawler:
+        crawler_config = json.load(crawler)
+
+    for url in crawler_config['base_urls']:
+        if not re.match('https://', url):
+            raise IncorrectURLError
+
+    if crawler_config['total_articles_to_find_and_parse'] > 100:
+        raise NumberOfArticlesOutOfRangeError
+
+    if not isinstance(crawler_config['total_articles_to_find_and_parse'], int):
+        raise IncorrectNumberOfArticlesError
+
+    return crawler_config['base_urls'], crawler_config['total_articles_to_find_and_parse']
 
 
 
 if __name__ == '__main__':
     # YOUR CODE HERE
-    response = requests.get('https://moyaokruga.ru/mayakdelty/Articles.aspx?articleId=434753')
-    if not response:
-        raise ImportError
-
-    print(response.headers)
