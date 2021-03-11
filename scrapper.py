3ab37db3ccce1a056a80aa8478f97a973a498918
yudakovaolesya <ovyudakova@edu.hse.ru>
2021-03-11 18:42:41 +0300

    changed

--- scrapper.py
+++ scrapper.py
@@ -142,8 +142,9 @@ def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    if not os.path.exists(base_path):
-        os.makedirs(base_path)
+    if os.path.exists(base_path):
+        os.remove(base_path)
+    os.makedirs(base_path)
 
 
 def validate_config(crawler_path):
