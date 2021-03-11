79f220396a0470203aa87c7e198baca163329432
Milana <milana.shxanukova@mail.ru>
2021-03-11 22:49:59 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,6 +7,5 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 ASSETS_PATH = os.path.join(PROJECT_ROOT, 'tmp', 'articles')
 CRAWLER_CONFIG_PATH = os.path.join(PROJECT_ROOT, 'crawler_config.json')
-HEADERS = {"User-Agent": 
-           "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
+HEADERS = {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Mobile Safari/537.36"}
