684b15112b79c22ce3724d288bdba2699a695181
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-03-12 20:22:17 +0300

    small rework

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,24 @@
 """
 Crawler implementation
 """
+from bs4 import BeautifulSoup
 import requests
+import time
+from urllib.parse import urljoin, urldefrag
+
+
+class LinkWorker:
+    def __init__(self, page, relative_link):
+        self.page = page
+        self.relative_link = relative_link
+
+    def get_absolute_link(self):
+        result = urljoin(self.page, self.relative_link)
+        return result
+
+    def update_link(self, url):
+        self.relative_link = url
+
 
 class IncorrectURLError(Exception):
     """
@@ -17,43 +34,69 @@ class NumberOfArticlesOutOfRangeError(Exception):
     """
     pass
 
+
 class IncorrectNumberOfArticlesError(Exception):
     """
     Custom error
     """
     pass
 
+
 class Crawler:
     """
     Crawler implementation
     """
+    lw = LinkWorker('', '')
+
     def __init__(self, seed_urls: list, max_articles: int):
         self.seed_urls = seed_urls
         self.max_articles = max_articles
 
     @staticmethod
     def _extract_url(article_bs):
-        pass
+        new_seed_urls = []
+        for a in article_bs.find_all('a'):
+            link = str(a.get('href'))
+            if link.find('https://') == -1:
+                try:
+                    Crawler.lw.update_link(link)
+                    new_seed_urls.append(Crawler.lw.get_absolute_link())
+                except:
+                    pass
+            else:
+                try:
+                    new_seed_urls.append(link)
+                except:
+                    pass
 
     def find_articles(self):
         """
         Finds articles
         """
-        raise IncorrectURLError
+        new_urls = []
 
-
+        for url in self.get_search_urls():
+            time.sleep(0.25)
+            try:
+                response = requests.get(self.get_search_urls())
+                parser = BeautifulSoup(response.text, 'lxml')
+                self.lw = LinkWorker(url, "")
+                new_urls.extend(self._extract_url(parser))
+            except Exception:
+                raise IncorrectURLError
 
     def get_search_urls(self):
         """
         Returns seed_urls param
         """
-        return seed_urls
+        return self.seed_urls
 
 
 class ArticleParser:
     """
     ArticleParser implementation
     """
+
     def __init__(self, full_url: str, article_id: int):
         pass
 
