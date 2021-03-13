d2ad1579cf6ec6c98a1f704e316c64455c3c7107
asbabiy <asbabiy@edu.hse.ru>
2021-03-13 11:15:23 +0300

    final release but some recent tests are incompatible with my implemetation

--- scrapper.py
+++ scrapper.py
@@ -2,27 +2,34 @@
 Crawler implementation
 """
 import json
-import os
-import re
-import time
-import random
-import shutil
 import logging.config
-
-from json.decoder import JSONDecodeError
+import os
+import pickle
+import random
+import re
+import shutil
+import time
 from collections import namedtuple
 from datetime import datetime
-from typing import Set, List, Tuple, Dict, Any
+from json.decoder import JSONDecodeError
 from pathlib import Path
+from typing import Any, Dict, List, Set, Tuple
 
 import requests
 from bs4 import BeautifulSoup
 from requests import RequestException
 
-from constants import CRAWLER_CONFIG_PATH, HEADERS, COOKIES, ASSETS_PATH, CRAWLER_STATE, PARSER_STATE
 from article import Article
+from constants import (
+    ASSETS_PATH,
+    COOKIES,
+    CRAWLER_CONFIG_PATH,
+    CRAWLER_STATE,
+    HEADERS,
+    PARSER_STATE,
+)
 
-logging.config.fileConfig(fname='crawler_logging.ini', disable_existing_loggers=False)
+logging.config.fileConfig(fname="crawler_logging.ini", disable_existing_loggers=False)
 log = logging.getLogger(__name__)
 
 
@@ -54,6 +61,7 @@ class Crawler:
     """
     Crawler implementation
     """
+
     seed_urls: List[str]
     max_articles: int
     max_articles_per_seed: int
@@ -95,8 +103,8 @@ class Crawler:
         """
         Extracts news urls from the seed page
         """
-        news = article_bs.select('div.col-md-9 div.col-md-6 h2 a')
-        links = [article.attrs['href'] for article in news]
+        news = article_bs.select("div.col-md-9 div.col-md-6 h2 a")
+        links = [article.attrs["href"] for article in news]
 
         return links
 
@@ -104,10 +112,10 @@ class Crawler:
         """
         Extracts specified number of links from the seed page and returns them
         """
-        soup = BeautifulSoup(page, 'lxml')
+        soup = BeautifulSoup(page, "lxml")
 
-        seed_articles = self._extract_url(soup)[:self.max_articles_per_seed]
-        links = seed_articles[:self.max_articles - len(self.urls)]
+        seed_articles = self._extract_url(soup)[: self.max_articles_per_seed]
+        links = seed_articles[: self.max_articles - len(self.urls)]
 
         return links
 
@@ -116,6 +124,7 @@ class CrawlerRecursive(Crawler):
     """
     Recursive Crawler
     """
+
     def find_articles(self):
         """
         Finds articles
@@ -145,34 +154,28 @@ class CrawlerRecursive(Crawler):
         seed = self.seed_urls[0]
         page = fetch_page(seed)
 
-        soup = BeautifulSoup(page, 'lxml')
-        next_page = soup.select_one('ul.pagination li:has(span.current) + li a')
+        soup = BeautifulSoup(page, "lxml")
+        next_page = soup.select_one("ul.pagination li:has(span.current) + li a")
 
-        return seed, next_page['href']
+        return seed, next_page["href"]
 
     def _save_state(self) -> None:
-        with open(CRAWLER_STATE, "w", encoding='utf-8') as file:
-            json.dump(
-                {
-                    'urls': sorted(self.urls),
-                    'seed': self.seed_urls
-                },
-                file,
-                indent=4
-            )
+        with open(CRAWLER_STATE, "wb") as file:
+            pickle.dump({"urls": self.urls, "seed": self.seed_urls}, file)
 
     def _load_state(self) -> None:
         if os.path.exists(CRAWLER_STATE):
-            with open(CRAWLER_STATE, encoding='utf-8') as file:
-                status: Dict[str, Any] = json.load(file)
+            with open(CRAWLER_STATE, "rb") as file:
+                status: Dict[str, Any] = pickle.load(file)
 
-            self.seed_urls, self.urls = status['seed'], set(status['urls'])
+            self.seed_urls, self.urls = status["seed"], status["urls"]
 
 
 class ArticleParser:
     """
     ArticleParser implementation
     """
+
     article: Article
     full_url: str
     article_id: int
@@ -189,55 +192,66 @@ class ArticleParser:
         """
         return datetime.fromisoformat(date_str)
 
-    def parse(self) -> None:
+    def parse(self):
         """
         Parses each article
         """
         if self.full_url in self._load_state():
-            return
+            log.info(
+                "Article #%s '%s' is duplicated. Skipping.",
+                self.article_id,
+                self.full_url,
+            )
+            return None
 
         page = fetch_page(self.full_url)
-        soup = BeautifulSoup(page, features='lxml')
+        soup = BeautifulSoup(page, features="lxml")
 
         self._fill_article_with_text(soup)
         self._fill_article_with_meta_information(soup)
 
         self._save_state()
 
-    def save(self):
-        self.article.save_raw()
+        log.info("Article #%s '%s' is processed.", self.article_id, self.full_url)
+
+        return self.article
 
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
-        self.article.text = '\n'.join([p.text for p in article_soup.select('.entry-content > p')])
+        self.article.text = "\n".join(
+            [p.text for p in article_soup.select(".entry-content > p")]
+        )
 
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
-        self.article.title = article_soup.select_one('h1').text
-        self.article.author = article_soup.select_one('li.autor').text
+        self.article.title = article_soup.select_one("h1").text
+        self.article.author = article_soup.select_one("li.autor").text
 
-        date: str = article_soup.select_one('time.entry-date').attrs['datetime']
+        date: str = article_soup.select_one("time.entry-date").attrs["datetime"]
         self.article.date = self.unify_date_format(date)
 
-        self.article.topics = [tag.text for tag in article_soup.select('a[rel^="category"]')]
+        self.article.topics = [
+            tag.text for tag in article_soup.select('a[rel^="category"]')
+        ]
 
     def _save_state(self):
         if not os.path.exists(PARSER_STATE):
-            with open(PARSER_STATE, 'w', encoding='utf-8') as file:
-                file.write('{"processed": []}')
+            with open(PARSER_STATE, "wb") as file:
+                initial_state = {"processed": set()}
+                pickle.dump(initial_state, file)
 
-        with open(PARSER_STATE, encoding='utf-8') as file:
-            state = json.load(file)
-            state['processed'].append(self.article.url)
+        with open(PARSER_STATE, "rb") as file:
+            state = pickle.load(file)
+            state["processed"].add(self.article.url)
 
-        with open(PARSER_STATE, 'w', encoding='utf-8') as file:
-            json.dump(state, file, indent=4)
+        with open(PARSER_STATE, "wb") as file:
+            pickle.dump(state, file)
 
     @staticmethod
     def _load_state():
         if os.path.exists(PARSER_STATE):
-            with open(PARSER_STATE, encoding='utf-8') as file:
-                state: Dict[str, list] = json.load(file)
-            return set(state['processed'])
-        return {}
+            with open(PARSER_STATE, "rb") as file:
+                state: Dict[str, list] = pickle.load(file)
+            return state["processed"]
+        return set()
 
 
 def fetch_page(url: str):
@@ -250,7 +264,9 @@ def fetch_page(url: str):
         return response.text
 
     except (RequestException, ConnectionError) as exc:
-        log.exception("%s was encountered while getting seed_urls", exc.__class__.__name__)
+        log.exception(
+            "%s was encountered while getting seed_urls", exc.__class__.__name__
+        )
 
 
 def prepare_environment(base_path: str) -> None:
@@ -268,14 +284,14 @@ def validate_config(crawler_path: str) -> Tuple[List[str], int, int]:
     with open(crawler_path) as file:
         config: dict = json.load(file)
 
-    Check = namedtuple('Check', ['status', 'error'])
+    Check = namedtuple("Check", ["status", "error"])
 
     try:
-        total_num = config['total_articles_to_find_and_parse']
-        url_check = re.compile(r'^(https?://)?[0-9a-z]+\.[-_0-9a-z]+\.[0-9a-z/]+', re.I)
+        total_num = config["total_articles_to_find_and_parse"]
+        url_check = re.compile(r"^(https?://)?[0-9a-z]+\.[-_0-9a-z]+\.[0-9a-z/]+", re.I)
 
         is_correct_total_num = isinstance(total_num, int)
-        is_correct_url = all(url_check.match(str(x)) for x in config['base_urls'])
+        is_correct_url = all(url_check.match(str(x)) for x in config["base_urls"])
         is_num_not_oor = is_correct_total_num and 0 < total_num <= 100000
 
         checks = (
@@ -286,40 +302,44 @@ def validate_config(crawler_path: str) -> Tuple[List[str], int, int]:
 
         for check in checks:
             if not check.status:
-                raise check.error('Could not check config.')
+                raise check.error("Could not check config.")
 
         return (
-            config['base_urls'],
-            config['total_articles_to_find_and_parse'],
-            config['max_number_articles_to_get_from_one_seed']
+            config["base_urls"],
+            config["total_articles_to_find_and_parse"],
+            config["max_number_articles_to_get_from_one_seed"],
         )
 
     except (JSONDecodeError, KeyError) as exc:
-        log.exception("%s was encountered while validating crawler config.", exc.__class__.__name__)
+        log.exception(
+            "%s was encountered while validating crawler config.",
+            exc.__class__.__name__,
+        )
         raise UnknownConfigError from exc
 
 
 def main():
     urls, max_num_articles, max_per_seed = validate_config(CRAWLER_CONFIG_PATH)
-    crawler = CrawlerRecursive(seed_urls=urls, max_articles=max_num_articles, max_articles_per_seed=max_per_seed)
+    crawler = CrawlerRecursive(
+        seed_urls=urls,
+        max_articles=max_num_articles,
+        max_articles_per_seed=max_per_seed,
+    )
 
     if not os.path.exists(ASSETS_PATH):
         prepare_environment(ASSETS_PATH)
 
     crawler.find_articles()
 
-    for idx, article_url in enumerate(sorted(crawler.urls)):
+    for idx, article_url in enumerate(sorted(crawler.urls), 1):
         parser = ArticleParser(full_url=article_url, article_id=idx)
 
-        time.sleep(random.uniform(2, 4))
-
-        parser.parse()
-        parser.save()
-
-        log.info("Article #%s '%s' is processed.", idx, article_url)
+        if article := parser.parse():
+            article.save_raw()
+            time.sleep(random.uniform(2, 4))
 
     log.info("Total: %s articles.", len(crawler.urls))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
