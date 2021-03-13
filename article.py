d09c9f3b2ccdba61f7dced1578efa425c212d9c4
marina-kaz <isthismeowgoingfrommyheart@gmail.com>
2021-03-13 14:35:44 +0300

    fixed date format

--- article.py
+++ article.py
@@ -99,7 +99,7 @@ class Article:
         """
         Converts datetime object to text
         """
-        return self.date.strftime("%Y-%m-%d")
+        return self.date.strftime("%Y-%m-%d %H:%M:%S")
 
     def _get_raw_text_path(self):
         """
