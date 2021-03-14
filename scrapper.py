1378ff3486ce85320cf83db72a26947a084e1abd
katerkr <ekrivenkova2001@mail.ru>
2021-03-14 14:40:50 +0300

    'tryyyy'

--- scrapper.py
+++ scrapper.py
@@ -149,16 +149,17 @@ def validate_config(crawler_path):
 
 if __name__ == '__main__':
     # YOUR CODE HERE
+    seed_urls, max_articles, max_articles_per_seed = validate_config(CRAWLER_CONFIG_PATH)
+
+    crawler = Crawler(seed_urls, max_articles, max_articles_per_seed)
+    urls = crawler.find_articles()
     prepare_environment(ASSETS_PATH)
+    article_id = 0
+    for article_url in urls:
+        article_id += 1
+        parser = ArticleParser(article_url, article_id)
+        sleep(5)
+        article = parser.parse()
+        article.save_raw()
 
-    urls_list, max_articles_num, max_articles_num_per_seed = validate_config(CRAWLER_CONFIG_PATH)
 
-    crawler = Crawler(seed_urls=urls_list,
-                      max_articles=max_articles_num,
-                      max_articles_per_seed=max_articles_num)
-    crawler.find_articles()
-
-    for article_id_n, article_url in enumerate(crawler.urls, 1):
-        parser = ArticleParser(full_url=article_url, article_id=article_id_n)
-        parsed_article = parser.parse()
-        parsed_article.save_raw()
