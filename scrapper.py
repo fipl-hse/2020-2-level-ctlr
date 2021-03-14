f5cef3ac0aa4e52ac1b29216fe600b82f15f3d3e
vchistyakova <65254510+vchistyakova@users.noreply.github.com>
2021-03-14 21:57:10 +0300

    changes(5)

--- scrapper.py
+++ scrapper.py
@@ -11,7 +11,8 @@ import os
 
 
 headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)' 'Chrome/88.0.4324.190 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)' 
+                      'Chrome/88.0.4324.190 Safari/537.36'
 }
 
 class IncorrectURLError(Exception):
