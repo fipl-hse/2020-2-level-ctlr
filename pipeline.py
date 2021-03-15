f46ecdc7e676b1e3ff448d5c6711665e069a1be7
katerinamerkulova <59052477+katerinamerkulova@users.noreply.github.com>
2021-03-15 14:00:57 +0300

    remove pass and whitespaces

--- pipeline.py
+++ pipeline.py
@@ -26,7 +26,6 @@ class MorphologicalToken:
         """
         Converts instance to str format
         """
-        pass
 
     def __str__(self):
         pass
@@ -43,19 +42,16 @@ class CorpusManager:
         """
         Gets article metadata
         """
-        pass
 
     def get_raw_text(self, text_id):
         """
         Opens processed text
         """
-        pass
 
     def write_processed_text(self, text_id, processed_text):
         """
         Writes processed text
         """
-        pass
 
 
 class TextProcessingPipeline:
@@ -69,28 +65,25 @@ class TextProcessingPipeline:
         """
         Runs pipeline process scenario
         """
-        pass
 
     @staticmethod
     def normalize_and_tag_text(text) -> str:
         """
         Processes each token and creates MorphToken class instance
         """
-        pass
 
     @staticmethod
     def transform_tokens_to_text(tokens: list) -> str:
         """
         Transforms given list of tokens to str
         """
-        pass
 
 
 def validate_given_path(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+    return path_to_validate
 
 
 if __name__ == "__main__":
