69bb24b3c5bf53f9d04d272c1ba7ce6b572a1536
daniilzelekson <64979241+daniilzelekson@users.noreply.github.com>
2021-03-18 20:51:31 +0300

    small rework

--- scrapper.py
+++ scrapper.py
@@ -146,9 +146,8 @@ def validate_config(crawler_path):
     except:
         raise IncorrectURLError('error')
     for url in json_dict['base_urls']:
-        try:
-            requests.get(url)
-        except Exception:
+        s = str(url)
+        if s.find('http') == -1:
             raise IncorrectURLError('error')
 
     return json_dict['base_urls'], ['total_articles_to_find_and_parse'], ['max_number_articles_to_get_from_one_seed']
