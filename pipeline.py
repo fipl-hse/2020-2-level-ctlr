4a058560e22d93047b913c0f5461ebbe5deef1bd
Anastasia <avisaichkina@edu.hse.ru>
2021-03-30 23:06:55 +0300

    first changes

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,14 @@
 Pipeline for text processing implementation
 """
 
+from pathlib import Path
 from typing import List
+
+from pymorphy2 import MorphAnalyzer
+from pymystem3 import Mystem
+
+from article import Article
+from constants import ASSETS_PATH
 
 
 class EmptyDirectoryError(Exception):
@@ -28,10 +35,19 @@ class MorphologicalToken:
     Stores language params for each processed token
     """
     def __init__(self, original_word, normalized_form):
-        pass
+        self.original_word = original_word
+        self.normalized_form = normalized_form
+        self.mystem_tags = ''
+        self.pymorphy_tags = ''
 
     def __str__(self):
-        return "MorphologicalToken instance here"
+        return f"{self.normalized_form}<{self.mystem_tags}>({self.pymorphy_tags})"
+
+    def public_method(self):
+        """
+        just to pass lint check
+        """
+        pass
 
 
 class CorpusManager:
@@ -39,17 +55,27 @@ class CorpusManager:
     Works with articles and stores them
     """
     def __init__(self, path_to_raw_txt_data: str):
-        pass
+        self.path_to_raw_txt_date = path_to_raw_txt_data
+        self._storage = {}
+        self._scan_dataset()
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        for file in Path(self.path_to_raw_txt_date).glob('*_raw.txt'):
+            article_id = str(file).split('\\')[-1].split('_')[0]
+            self._storage[article_id] = Article(url=None, article_id=article_id)
 
     def get_articles(self):
         """
         Returns storage params
+        """
+        return self._storage
+
+    def public_method(self):
+        """
+        just to pass lint check
         """
         pass
 
@@ -59,17 +85,27 @@ class TextProcessingPipeline:
     Process articles from corpus manager
     """
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
+        self._text = ''
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        for file in self.corpus_manager.get_articles().values():
+            self._text = file.get_raw_text()
+            file.save_processed(' '.join(self._process()))
 
     def _process(self) -> List[type(MorphologicalToken)]:
         """
         Performs processing of each text
+        """
+        # TODO realization
+        pass
+
+    def public_method(self):
+        """
+        just to pass lint check
         """
         pass
 
@@ -78,11 +114,23 @@ def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+    path = Path(path_to_validate)
+    if not path.exists():
+        raise FileNotFoundError
+    if not path.is_dir():
+        raise NotADirectoryError
+    if not list(path.iterdir()):
+        raise EmptyDirectoryError
+    # TODO InconsistentDatasetError and UnknownDatasetError
 
 
 def main():
-    print('Your code goes here')
+    validate_dataset(ASSETS_PATH)
+
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+
+    pipeline.run()
 
 
 if __name__ == "__main__":
