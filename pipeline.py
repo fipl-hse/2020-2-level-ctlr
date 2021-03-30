08d8a81652e57ac5a5b1c6c5bdab41c2ab2421e5
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2021-03-30 13:37:10 +0300

    pipeline mark 8 implimentation v1.0

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,14 @@
 Pipeline for text processing implementation
 """
 
+from pathlib import Path
 from typing import List
+
+from pymorphy2 import MorphAnalyzer
+from pymystem3 import Mystem
+
+from article import Article
+from constants import ASSETS_PATH
 
 
 class EmptyDirectoryError(Exception):
@@ -28,10 +35,13 @@ class MorphologicalToken:
     Stores language params for each processed token
     """
     def __init__(self, original_word, normalized_form):
-        pass
+        self.mystem_tags = ''
+        self.pymorphy_tags = ''
+        self.original_word = original_word
+        self.normalized_form = normalized_form
 
     def __str__(self):
-        return "MorphologicalToken instance here"
+        return f"{self.normalized_form}<{self.mystem_tags}>({self.pymorphy_tags})"
 
 
 class CorpusManager:
@@ -39,19 +49,23 @@ class CorpusManager:
     Works with articles and stores them
     """
     def __init__(self, path_to_raw_txt_data: str):
-        pass
+        self.path_to_raw_txt_data = path_to_raw_txt_data
+        self._storage = {}
+        self._scan_dataset()
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        for file in Path(self.path_to_raw_txt_data).glob('*_raw.txt'):
+            file_id = str(file).split('\\')[-1].split('_')[0]
+            self._storage[file_id] = Article(url=None, article_id=file_id)
 
     def get_articles(self):
         """
         Returns storage params
         """
-        pass
+        return self._storage
 
 
 class TextProcessingPipeline:
@@ -59,30 +73,63 @@ class TextProcessingPipeline:
     Process articles from corpus manager
     """
     def __init__(self, corpus_manager: CorpusManager):
-        pass
+        self.corpus_manager = corpus_manager
+        self._text = ''
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        for article in self.corpus_manager.get_articles().values():
+            self._text = article.get_raw_text()
+            tokens = self._process()
+            article.save_processed(' '.join(tokens))
 
     def _process(self) -> List[type(MorphologicalToken)]:
         """
         Performs processing of each text
         """
-        pass
+        tokens = []
+        result = Mystem().analyze(self._text)
+        for word in result:
+            try:
+                token = MorphologicalToken(original_word=word['text'].lower(),
+                                           normalized_form=word['analysis'][0]['lex'])
+                token.mystem_tags = word['analysis'][0]['gr']
+                token.pymorphy_tags = MorphAnalyzer().parse(word['text'])[0].tag
+            except (KeyError, IndexError):
+                if word['text'].isalnum():
+                    token = MorphologicalToken(original_word=word['text'].lower(),
+                                               normalized_form=word['text'].lower())
+                else:
+                    continue
+            tokens.append(str(token))
+        return tokens
 
 
 def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+    # FileNotFoundError, NotADirectoryError, InconsistentDatasetError, EmptyDirectoryError, UnknownDatasetError
+    path = Path(path_to_validate)
+
+    if not path.is_dir():
+        if path.is_file():
+            raise NotADirectoryError
+        else:
+            raise FileNotFoundError
+
+    if not any(path.iterdir()):
+        raise EmptyDirectoryError
 
 
 def main():
-    print('Your code goes here')
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
