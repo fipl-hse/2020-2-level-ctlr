31c8db512d7a515444e1a7b9ece21a15dd5d3b79
amksmva <anya.maksimova.1509@mail.ru>
2021-04-01 17:06:25 +0300

    some changes, scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -108,8 +108,6 @@ class ArticleParser:
         Parses each article
         """
         response = requests.get(self.full_url, headers=headers)
-        if not response:
-            raise IncorrectURLError
         article_soup = BeautifulSoup(response.content, features='lxml')
         self.article.text += self._fill_article_with_text(article_soup)
         self._fill_article_with_meta_information(article_soup)
