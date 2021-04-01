bae61fd62086baadaf46ac7270d3462a4e887ea3
yudakovaolesya <ovyudakova@edu.hse.ru>
2021-04-01 18:13:33 +0300

    6 score

--- pipeline.py
+++ pipeline.py
@@ -2,12 +2,12 @@
 Pipeline for text processing implementation
 """
 
-from typing import List
 import os
-import article
 from pathlib import Path
+from typing import List
 from pymystem3 import Mystem
-from constants import ASSETS_PATH  # распределить !!!!!!!!!!
+import article
+from constants import ASSETS_PATH
 
 
 class EmptyDirectoryError(Exception):
@@ -39,8 +39,7 @@ class MorphologicalToken:
         self.pymorphy_tags = ''
 
     def __str__(self):
-        return "MorphologicalToken instance here"
-        # return "{}<{}>({})".format(self.normalized_form, self.mystem_tags, self.pymorphy_tags)
+        return "{}<{}>({})".format(self.normalized_form, self.mystem_tags, self.pymorphy_tags)
 
 
 class CorpusManager:
@@ -57,11 +56,9 @@ class CorpusManager:
         """
         Register each dataset entry
         """
-        data_content = os.listdir(self.path_to_data)
-        for file_content in data_content:
-            if '_raw' in file_content:
-                key_number = int(file_content[0])
-                self._storage[key_number] = article.Article(url=None, article_id=key_number)
+        for one_file in Path(self.path_to_data).rglob('*_raw.text'):
+            key_number = int(one_file.name.split('_')[0])
+            self._storage[key_number] = article.Article(url=None, article_id=key_number)
 
     def get_articles(self):
         """
@@ -76,18 +73,31 @@ class TextProcessingPipeline:
     """
     def __init__(self, corpus_manager: CorpusManager):
         self.corpus_manager = corpus_manager
+        self.raw_text = ''
 
     def run(self):
         """
         Runs pipeline process scenario
         """
-        pass
+        articles = self.corpus_manager.get_articles()
+        for one_article in articles.values():
+            self.raw_text = one_article.get_raw_text()
+            tokens = self._process()
+            one_article.save_processed(' '.join(tokens))
 
     def _process(self) -> List[type(MorphologicalToken)]:
         """
         Performs processing of each text
         """
-        pass
+        result = Mystem().analyze(self.raw_text)
+        tokens = []
+        for analyzed_word in result:
+            if analyzed_word.get('analysis'):  # чтобы были только слова
+                token = MorphologicalToken(original_word=analyzed_word['text'].lower(),
+                                           normalized_form=analyzed_word['analysis'][0]['lex'])
+                token.mystem_tags = analyzed_word['analysis'][0]['gr']
+                tokens.append(str(token))
+        return tokens
 
 
 def validate_dataset(path_to_validate):
@@ -102,25 +112,29 @@ def validate_dataset(path_to_validate):
 
     if os.path.exists(path_to_validate):
 
+        if not Path(path_to_validate).is_dir():
+            raise NotADirectoryError
+
         dir_content = os.listdir(path_to_validate)
         if not dir_content:
             raise EmptyDirectoryError
 
-        if not Path(path_to_validate).is_dir():
-            raise NotADirectoryError
-
-        #if
-           # raise InconsistentDatasetError
-
-    return None
+        files = []
+        files_ids = []
+        for one_file in dir_content:
+            if '_raw.txt' in str(one_file):
+                files.append(one_file)
+                files_ids.append(int(str(one_file)[0]))
+        if len(files) != len(files_ids) or set(files_ids) != set(range(1, len(files) + 1)):
+            raise InconsistentDatasetError
 
 
 def main():
     print('Your code goes here')
     validate_dataset(ASSETS_PATH)  # step 1
     corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)  # step 2
-    token = MorphologicalToken(original_word, normalized_form)  # step 3 ???
     pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)  # step 4
+    pipeline.run()
 
 
 if __name__ == "__main__":
