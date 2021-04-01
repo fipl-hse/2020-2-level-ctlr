2d5fe6330df604a7f857fc9ddcfa9168484d6673
blackberrybomb <65188682+blackberrybomb@users.noreply.github.com>
2021-04-01 19:26:31 +0300

    Nado ubrat' svoi greshnye ruki ot programmirovania

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,14 @@
 """
 Pipeline for text processing implementation
 """
-
+import os
 from typing import List
+
+import pymorphy2
+from pymystem3 import Mystem
+
+from article import Article
+from constants import ASSETS_PATH
 
 
 class EmptyDirectoryError(Exception):
@@ -28,7 +34,10 @@ class MorphologicalToken:
     Stores language params for each processed token
     """
     def __init__(self, original_word, normalized_form):
-        pass
+        self.original_word = original_word
+        self.normalized_form = normalized_form
+        self.mystem_tags = ''
+        self.pymorpy_tags = ''
 
     def __str__(self):
         return "MorphologicalToken instance here"
@@ -39,19 +48,23 @@ class CorpusManager:
     Works with articles and stores them
     """
     def __init__(self, path_to_raw_txt_data: str):
-        pass
+        self.path_to_raw_txt_data = path_to_raw_txt_data
+        self._storage = {}
+        self._scan_dataset()
 
     def _scan_dataset(self):
         """
         Register each dataset entry
         """
-        pass
+        last = len(os.listdir(self.path_to_raw_txt_data)) // 2
+        for i in range(1, last + 1):
+            self._storage[i] = Article(url=None, article_id=i)
 
     def get_articles(self):
         """
         Returns storage params
         """
-        pass
+        return self._storage
 
 
 class TextProcessingPipeline:
@@ -59,6 +72,7 @@ class TextProcessingPipeline:
     Process articles from corpus manager
     """
     def __init__(self, corpus_manager: CorpusManager):
+
         pass
 
     def run(self):
@@ -78,11 +92,32 @@ def validate_dataset(path_to_validate):
     """
     Validates folder with assets
     """
-    pass
+    if not os.path.exists(path_to_validate):
+        raise FileNotFoundError
+
+    files = os.listdir(path_to_validate)
+    if not files:
+        raise EmptyDirectoryError
+
+    if not os.path.isdir(path_to_validate):
+        raise NotADirectoryError
+
+    if len(files) % 2:
+        raise InconsistentDatasetError
+
+    files_number = len(files) // 2
+    for i in range(1, files_number + 1):
+        if not (os.path.exists(os.path.join(path_to_validate, f'{i}_meta.json'))
+                or os.path.exists(os.path.join(path_to_validate, f'{i}_raw.txt'))):
+            raise InconsistentDatasetError
 
 
 def main():
-    print('Your code goes here')
+    # print('Your code goes here')
+    validate_dataset(ASSETS_PATH)
+    corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
+    pipeline = TextProcessingPipeline(corpus_manager=corpus_manager)
+    pipeline.run()
 
 
 if __name__ == "__main__":
