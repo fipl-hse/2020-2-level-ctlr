2ac2c74d56ffacf6b55321632fcff3c07ba42516
katerkr <ekrivenkova2001@mail.ru>
2021-04-02 19:47:27 +0300

    'fix'

--- scrapper.py
+++ scrapper.py
@@ -150,16 +150,13 @@ def validate_config(crawler_path):
 
 if __name__ == '__main__':
     # YOUR CODE HERE
-    seed_urls, max_articles, max_articles_per_seed = validate_config(CRAWLER_CONFIG_PATH)
-
-    crawler = Crawler(seed_urls, max_articles, max_articles_per_seed)
-    crawler.find_articles()
-
+    urls, maximum_articles, maximum_articles_per_seed = validate_config(CRAWLER_CONFIG_PATH)
+    crawler = Crawler(urls, maximum_articles, maximum_articles_per_seed)
+    articles = crawler.find_articles()
     prepare_environment(ASSETS_PATH)
 
     for ind, article_url in enumerate(crawler.urls):
-        parser = ArticleParser(full_url=article_url, article_id=ind + 1)
+        parser = ArticleParser(full_url=article_url, article_id=ind+1)
         sleep(5)
         article = parser.parse()
         article.save_raw()
-
