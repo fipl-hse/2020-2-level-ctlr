b528d42cba574564bb9041871e446b41735977fb
alenashuvar <alenashuvar@users.noreply.github.com>
2021-04-02 13:23:41 +0300

    change realization for 10

--- pos_frequency_pipeline.py
+++ pos_frequency_pipeline.py
@@ -1,3 +1,4 @@
+# pylint: disable=R0903
 """
 Implementation of POSFrequencyPipeline for score ten only.
 """
@@ -5,8 +6,6 @@ Implementation of POSFrequencyPipeline for score ten only.
 import json
 from pathlib import Path
 import re
-
-from pymystem3 import Mystem
 
 from constants import ASSETS_PATH
 from pipeline import CorpusManager
@@ -25,27 +24,22 @@ class POSFrequencyPipeline:
         """
         articles = self.corpus_manager.get_articles()
         for proc_article in articles.values():
-            self.text = proc_article.get_raw_text()
-            self.pos_freq = self._process()
+            path = Path(ASSETS_PATH) / f"{proc_article.article_id}_processed.txt"
+            with open(path, encoding='utf-8') as file:
+                self.text = file.read()
+            self.pos_freq = self._count_freq()
             self._write_freq_meta(proc_article.article_id)
             visualize(statistics=self.pos_freq,
                       path_to_save=Path(ASSETS_PATH) / f"{proc_article.article_id}_image.png")
 
-    def _analyze_text(self):
-        """"
-        Analyzes a text
-        """
-        return Mystem().analyze(self.text)
-
-    def _process(self):
+    def _count_freq(self):
         """"
         Counts frequencies for parts of speech in a text
         """
         pos_frequencies = {}
-        for token in self._analyze_text():
-            if token.get('analysis'):
-                found_pos = re.findall(r"^[A-Z]+", token["analysis"][0]["gr"])
-                pos_frequencies[found_pos[0]] = pos_frequencies.get(found_pos[0], 0) + 1
+        found_pos = re.findall(r"^[A-Z]+", self.text)
+        for pos in found_pos:
+            pos_frequencies[pos] = pos_frequencies.get(pos, 0) + 1
         return pos_frequencies
 
     def _write_freq_meta(self, art_id):
