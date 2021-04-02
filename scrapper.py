2b41e5f9490c5638d4428e88b2d27bdf2cfe8e84
Anastasia <avisaichkina@edu.hse.ru>
2021-04-02 16:12:04 +0300

    still don't know what's going on

--- scrapper.py
+++ scrapper.py
@@ -168,8 +168,8 @@ if __name__ == '__main__':
     crawler.find_articles()
     prepare_environment(constants.PROJECT_ROOT)
 
-    for i, url in enumerate(crawler.urls):
-        parser = ArticleParser(full_url=url, article_id=i + 1)
+    for i, url in enumerate(crawler.urls, 1):
+        parser = ArticleParser(full_url=url, article_id=i)
         article = parser.parse()
         article.save_raw()
         sleep(random.randrange(2, 6))
