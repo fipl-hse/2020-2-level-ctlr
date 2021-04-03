41e33459706527c3def74fcefadd51167e635243
LeraTormashova <65281894+LeraTormashova@users.noreply.github.com>
2021-04-03 16:57:18 +0300

    added parser

--- scrapper.py
+++ scrapper.py
@@ -2,11 +2,15 @@
 Crawler implementation
 """
 import json
+import os
+import shutil
 import requests
+import article
 from random import randint
 from bs4 import BeautifulSoup
 from time import sleep
-from constants import CRAWLER_CONFIG_PATH
+from datetime import datetime
+from constants import CRAWLER_CONFIG_PATH, ASSETS_PATH
 
 
 headers = {
@@ -86,33 +90,53 @@ class ArticleParser:
     ArticleParser implementation
     """
     def __init__(self, full_url: str, article_id: int):
-        pass
+        self.full_url = full_url
+        self.article_id = article_id
+        self.article = article.Article(url=full_url, article_id=article_id)
 
     def _fill_article_with_text(self, article_soup):
-        pass
+        article_text = article_soup.find('div', class_="entry-post").find_all('p')
+        article_list = [paragraph.text.strip() for paragraph in article_text]
+        self.article.text = '\n'.join(article_list)
 
     def _fill_article_with_meta_information(self, article_soup):
-        pass
+        self.article.title = article_soup.find('div', class_="head-post").find('h2').text.strip()
+
+        try:
+            self.article.author = article_soup.find('div', class_="author").find('a').text.strip()
+        except AttributeError:
+            self.article.author = 'NOT FOUND'
+
+        raw_date = article_soup.find('span', class_="time")['title']
+        self.article.date = self.unify_date_format(raw_date)
 
     @staticmethod
     def unify_date_format(date_str):
         """
         Unifies date format
         """
-        pass
+        unified_date = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S") # need to check
+        return unified_date
 
     def parse(self):
         """
         Parses each article
         """
-        pass
+        response = requests.get(url=self.full_url, headers=headers)
+        if response.status_code == 200:
+            article_bs = BeautifulSoup(response.content, features='lxml')
+            self._fill_article_with_text(article_bs)
+            self._fill_article_with_meta_information(article_bs)
 
+        return self.article
 
 def prepare_environment(base_path):
     """
     Creates ASSETS_PATH folder if not created and removes existing folder
     """
-    pass
+    if os.path.exists(base_path):
+        shutil.rmtree(base_path)
+    os.makedirs(base_path)
 
 
 def validate_config(crawler_path):
@@ -152,8 +176,19 @@ def validate_config(crawler_path):
     return urls, max_articles, max_articles_per_seed
 
 if __name__ == '__main__':
+    prepare_environment(ASSETS_PATH)
+
     urls, max_articles, max_articles_per_seed = validate_config(CRAWLER_CONFIG_PATH)
+
     crawler = Crawler(seed_urls=urls,
                       max_articles=max_articles,
                       max_articles_per_seed=max_articles_per_seed)
+
+    crawler.find_articles()
+
+    for id, url in enumerate(crawler.urls):
+        parser = ArticleParser(full_url=url, article_id=id + 1)
+        parsed_article = parser.parse()
+        parsed_article.save_raw()
+        sleep(randint(2, 6))
 
