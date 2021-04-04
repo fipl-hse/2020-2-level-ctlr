07e1b2abf7f6cab547fd2f389e433e1b1e9294dc
Anastasia <avisaichkina@edu.hse.ru>
2021-04-04 13:05:16 +0300

    return test

--- config/student_text_preprocess_score_eight_test.py
+++ config/student_text_preprocess_score_eight_test.py
@@ -1,7 +1,6 @@
 import re
 import os
 import unittest
-import shutil
 from constants import ASSETS_PATH
 
 
@@ -10,8 +9,6 @@ TAGS = ["A", "ADV", "S", "V", "PR", "ANUM", "CONJ", "SPRO", "APRO", "PART", "NUM
 
 class StudentTextPreprocessTest(unittest.TestCase):
     def setUp(self) -> None:
-        shutil.rmtree(ASSETS_PATH, ignore_errors=True)
-        os.mkdir(ASSETS_PATH)
         self.articles = dict()
         for article in os.listdir(ASSETS_PATH):
             if article.endswith("_processed.txt"):
