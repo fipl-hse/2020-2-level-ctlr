dc9e048df47dd4a11cb4c6797c97c8fbc4396be3
LeraTormashova <65281894+LeraTormashova@users.noreply.github.com>
2021-04-04 11:10:23 +0300

    one more change

--- pipeline.py
+++ pipeline.py
@@ -90,7 +90,7 @@ class TextProcessingPipeline:
         for article in self.corpus_manager.get_articles().values():
             self.raw_text_article = article.get_raw_text()
             tokens = self._process
-            article.save_processed(' '.join(map(str, tokens)))
+            article.save_processed(' '.join(tokens))
 
     def _process(self) -> List[type(MorphologicalToken)]:
         """
@@ -104,7 +104,7 @@ class TextProcessingPipeline:
                 token = MorphologicalToken(original_word=word['text'], normalized_form=word['analysis'][0]['lex'])
                 token.mystem_tags = word['analysis'][0]['gr']
                 token.pymorphy_tags = MorphAnalyzer().parse(token.original_word)[0].tag
-                morph_tokens.append(token)
+                morph_tokens.append(str(token))
         return morph_tokens
 
     def some_public_method(self):
