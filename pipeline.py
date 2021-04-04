4efc05d3e9608bcf9da39da34f661b52c8219b7f
marina-kaz <isthismeowgoingfrommyheart@gmail.com>
2021-04-04 15:01:42 +0300

    i am sorry for my terrible commit history

--- pipeline.py
+++ pipeline.py
@@ -129,7 +129,7 @@ def validate_dataset(path_to_validate):
         raise InconsistentDatasetError
     meta_indices = sorted(list(map(lambda x: int(x.split('_')[0]), metas)))
     raw_indices = sorted(list(map(lambda x: int(x.split('_')[0]), raws)))
-    if not meta_indices == raw_indices:
+    if not raw_indices == meta_indices:
         raise InconsistentDatasetError
 
 
